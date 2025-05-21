bd828957c846faa0eabf8154fa1113efcd6295a5
Jorge <jorgehbarrado@gmail.com>
2025-05-22 01:12:31 +0300

    Cargar fotos perfil
    -Se ha añadido una libreria q permite guardar las fotos en cache para q se cargen mas rapido

--- party_view/lib/widget/fotoPerfil.dart
+++ party_view/lib/widget/fotoPerfil.dart
@@ -1,36 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:party_view/provider/personaProvider.dart';
 import 'package:provider/provider.dart';
 
 class fotoPerfil extends StatelessWidget {
-  const fotoPerfil({
-    super.key,
-  });
+  const fotoPerfil({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final imagenes = [
+      "https://1drv.ms/i/c/f0a46d1dbb249072/IQRnWN3uXx_9QZE1hEEYpUWWAf-gmWac--x2INSSsA7geos?width=1024",
+      "https://1drv.ms/i/c/f0a46d1dbb249072/IQQHS8fWilNWQ47P18YcglaEAfmftJ25XC5-qrF-GXQ2F98?width=1024",
+      "https://1drv.ms/i/c/f0a46d1dbb249072/IQTkFcUcDx12SYqz178XAvr7AQap0z-COiTDhqdclfbLfFQ?width=1024",
+      "https://1drv.ms/i/c/f0a46d1dbb249072/IQRA-ky4gjbPSLv9Mdf8_1yQAUpVS3RcIxp0QGJDQMvAnQ0?width=1024",
+    ];
+
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         CircleAvatar(
           radius: 48,
-          backgroundImage: NetworkImage(
-            Provider.of<PersonaProvider>(context, listen: true).getPersona!.fotoUrl 
+          backgroundImage: CachedNetworkImageProvider(
+            Provider.of<PersonaProvider>(context, listen: true).getPersona!.url
           ),
           backgroundColor: Colors.grey[300],
         ),
         const SizedBox(width: 24),
         ElevatedButton(
           onPressed: () async {
-            final imagenes = [
-              "https://1drv.ms/i/c/f0a46d1dbb249072/IQRnWN3uXx_9QZE1hEEYpUWWAf-gmWac--x2INSSsA7geos?width=1024",
-              "https://1drv.ms/i/c/f0a46d1dbb249072/IQQHS8fWilNWQ47P18YcglaEAfmftJ25XC5-qrF-GXQ2F98?width=1024",
-              "https://1drv.ms/i/c/f0a46d1dbb249072/IQTkFcUcDx12SYqz178XAvr7AQap0z-COiTDhqdclfbLfFQ?width=1024",
-              "https://1drv.ms/i/c/f0a46d1dbb249072/IQRA-ky4gjbPSLv9Mdf8_1yQAUpVS3RcIxp0QGJDQMvAnQ0?width=1024",
-            ];
+            final personaProvider = Provider.of<PersonaProvider>(context, listen: false);
+
             showDialog(
               context: context,
-              builder: (context) {
+              builder: (dialogContext) {
                 return AlertDialog(
                   title: const Text("Elige tu imagen de perfil"),
                   content: SizedBox(
@@ -40,13 +42,12 @@ class fotoPerfil extends StatelessWidget {
                       runSpacing: 16,
                       children: imagenes.map((url) => GestureDetector(
                         onTap: () {
-                          final personaProvider = Provider.of<PersonaProvider>(context, listen: false);
                           personaProvider.setFotoUrl(url, context);
-                          Navigator.of(context).pop();
+                          Navigator.of(dialogContext).pop();
                         },
                         child: CircleAvatar(
                           radius: 36,
-                          backgroundImage: NetworkImage(url),
+                          backgroundImage: CachedNetworkImageProvider(url),
                         ),
                       )).toList(),
                     ),
